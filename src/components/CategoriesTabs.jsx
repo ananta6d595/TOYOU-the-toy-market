2e72fe8bc761c12729e05c35b1ebfe42cadacb59
Ananta Roy <ananta6d595@gmail.com>
2023-05-23 23:58:13 +0600

    tab toys working not

--- src/components/CategoriesTabs.jsx
+++ src/components/CategoriesTabs.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
-import { Tab } from "@headlessui/react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
 import TabToysCard from "./TabToysCard";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const CategoriesTabs = () => {
     const [toys, setToys] = useState([]);
+    let catToys = [];
 
     function classNames(...classes) {
         return classes.filter(Boolean).join(" ");
@@ -29,41 +31,23 @@ const CategoriesTabs = () => {
         <div
             className="container min-h-[400px] px-2 pb-16 sm:px-0"
             data-aos="fade-right">
-            <Tab.Group>
-                <Tab.List className="w-full max-w-2xl mx-auto flex space-x-1 rounded-xl bg-blue-900/20 p-1">
-                    {categories.map((category) => (
-                        <Tab
-                            key={category}
-                            className={({ selected }) =>
-                                classNames(
-                                    "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
-                                    "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
-                                    selected
-                                        ? "bg-white shadow"
-                                        : "text-cyan-100 hover:bg-white/[0.12] hover:text-white"
-                                )
-                            }>
-                            {category}
-                        </Tab>
+            <Tabs>
+                <TabList className="text-center">
+                    {categories.map((cat, index) => (
+                        <Tab key={index}>{cat}</Tab>
                     ))}
-                </Tab.List>
-                <Tab.Panels className="mt-2">
-                    {categories.map((category, idx) => (
-                        <Tab.Panel
-                            key={idx}
-                            className={classNames(
-                                "rounded-xl bg-white p-3",
-                                "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2 "
-                            )}>
-                            <div className="grid md:grid-cols-2">
-                                {toys.map((toy) => (
-                                    <TabToysCard toy={toy} key={toy._id}></TabToysCard>
-                                ))}
-                            </div>
-                        </Tab.Panel>
-                    ))}
-                </Tab.Panels>
-            </Tab.Group>
+                </TabList>
+
+                {categories.map((cat, index) => (
+                    <TabPanel key={index}>
+                        {toys
+                            .filter((toy) => toy.seller_name == cat)
+                            .map((toy) => (
+                                <TabToysCard toy={toy} key={toy._id}></TabToysCard>
+                            ))}
+                    </TabPanel>
+                ))}
+            </Tabs>
         </div>
     );
 };
